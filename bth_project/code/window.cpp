266c79edf44812f05caab29a6411174d293b81b2
Olle <ollenassen1992@gmail.com>
2019-03-15 15:09:17 +0100

    working

--- bth_project/code/window.cpp
+++ bth_project/code/window.cpp
@@ -4,10 +4,10 @@
 
 bool window_init(Window * window, const char* title, int width, int height)
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
 	window->window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		width, height, SDL_WINDOW_OPENGL);
-
-	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
